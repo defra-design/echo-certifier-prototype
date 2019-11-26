2175060ce9a772bb69bc5590586146aa8c45619f
Richard <richard.hatton@defra.gov.uk>
2019-11-26 08:10:23 +0000

    adding whole of 7006 journey, with updates to the certificate status, E2E

--- app/routes.js
+++ app/routes.js
@@ -5,4 +5,5 @@ const latest_version = "0-1"
 // Add your routes here - above the module.exports line
 
 require('./routes/1-3/routes.js')(router);
+require('./routes/1-4/routes.js')(router);
 module.exports = router
