df74044279b846c4913cc095b706ea48dfeffbbf
Richard <richard.hatton@defra.gov.uk>
2020-04-02 13:57:03 +0100

    version control, v6

---
+++ app/routes/1-6/qr-code.js
@@ -0,0 +1,19 @@
+module.exports = function(router) {
+  // Load helper functions
+  var tools = require('../tools.js')
+
+
+  // ADD extra routing here if needed.
+
+
+  // CHANGE VERSION TO THE VERSION
+  const version = '1-6'
+  const base_url = version + "/qr-code"
+  const file_url = version + "/certifier"
+
+
+
+  
+
+
+}
